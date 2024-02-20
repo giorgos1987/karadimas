c0ce51cb64e88e4bfd792f4e468f7580fcd90150
giorgos1987 <gpap3724@gmail.com>
2024-02-20 23:01:39 +0200

    Initial version of karadimastyres generated by generator-jhipster@7.9.2

---
+++ src/main/java/com/karadimas/tyres/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.karadimas.tyres.service.dto;
---
+++ src/test/java/com/karadimas/tyres/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.karadimas.tyres;
+
+import com.karadimas.tyres.KaradimastyresApp;
+import com.karadimas.tyres.config.AsyncSyncConfiguration;
+import com.karadimas.tyres.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { KaradimastyresApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
